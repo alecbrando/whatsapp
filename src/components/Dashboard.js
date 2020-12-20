e712f439a08525732df34e8145bc7ae77df736d8
alec <alecbgarcia@gmail.com>
2020-12-20 14:12:16 -0700

    conversations

---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+export default function Dashboard(props) {
+    return (
+        <div className="d-flex" style={{height: '100vh'}}>
+            <Sidebar id={props.id}></Sidebar>
+        </div>
+    )
+}

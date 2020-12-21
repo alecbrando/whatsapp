25c89a62b29ba1b19e8b9c8e9c0b1967b1b0eef2
alec <alecbgarcia@gmail.com>
2020-12-21 10:48:51 -0700

    able to send and recieve

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import { useConversations } from '../contexts/ConversationsProvider'
+import OpenConversation from './OpenConversation'
 export default function Dashboard(props) {
+    const { selectedConversation } = useConversations()
     return (
         <div className="d-flex" style={{height: '100vh'}}>
             <Sidebar id={props.id}></Sidebar>
+            {selectedConversation && <OpenConversation/>}
         </div>
     )
 }
